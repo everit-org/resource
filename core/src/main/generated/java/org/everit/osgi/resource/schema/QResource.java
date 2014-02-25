d2981cdf77076c75fccdf532db9ae8e6ff1a689e
mihaly.pajerich <mihaly.pajerich@everit.biz>
2014-02-25 16:14:35 +0100

    Component initial commit.

---
+++ core/src/main/generated/java/org/everit/osgi/resource/schema/QResource.java
@@ -0,0 +1,71 @@
+/**
+ * This file is part of Everit - Resource.
+ *
+ * Everit - Resource is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - Resource is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - Resource.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.everit.osgi.resource.schema;
+
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.path.*;
+
+import com.mysema.query.types.PathMetadata;
+import javax.annotation.Generated;
+import com.mysema.query.types.Path;
+
+import com.mysema.query.sql.ColumnMetadata;
+
+
+
+
+/**
+ * QResource is a Querydsl query type for QResource
+ */
+@Generated("com.mysema.query.sql.codegen.MetaDataSerializer")
+public class QResource extends com.mysema.query.sql.RelationalPathBase<QResource> {
+
+    private static final long serialVersionUID = -1131885723;
+
+    public static final QResource resource = new QResource("resource");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final com.mysema.query.sql.PrimaryKey<QResource> resourcePk = createPrimaryKey(id);
+
+    public QResource(String variable) {
+        super(QResource.class, forVariable(variable), "org.everit.osgi.resource.schema", "resource");
+        addMetadata();
+    }
+
+    public QResource(String variable, String schema, String table) {
+        super(QResource.class, forVariable(variable), schema, table);
+        addMetadata();
+    }
+
+    public QResource(Path<? extends QResource> path) {
+        super(path.getType(), path.getMetadata(), "org.everit.osgi.resource.schema", "resource");
+        addMetadata();
+    }
+
+    public QResource(PathMetadata<?> metadata) {
+        super(QResource.class, metadata, "org.everit.osgi.resource.schema", "resource");
+        addMetadata();
+    }
+
+    public void addMetadata() {
+        addMetadata(id, ColumnMetadata.named("id").ofType(-5).withSize(19).notNull());
+    }
+
+}
+
---
+++ tests/src/main/java/org/everit/osgi/resource/tests/ResourceTestComponent.java
@@ -0,0 +1,115 @@
+/**
+ * This file is part of Everit - Resource Tests.
+ *
+ * Everit - Resource Tests is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - Resource Tests is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - Resource Tests.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.everit.osgi.resource.tests;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferencePolicy;
+import org.apache.felix.scr.annotations.Service;
+import org.everit.osgi.dev.testrunner.TestDuringDevelopment;
+import org.everit.osgi.resource.api.ResourceService;
+import org.everit.osgi.resource.schema.QResource;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mysema.query.sql.SQLQuery;
+import com.mysema.query.sql.SQLTemplates;
+
+/**
+ * Test component for testing {@link ResourceService} methods.
+ */
+@Component(name = "ResourceTest", immediate = true, metatype = true, policy = ConfigurationPolicy.REQUIRE)
+@Service(value = ResourceTestComponent.class)
+@Properties({ @Property(name = "eosgi.testEngine", value = "junit4"),
+        @Property(name = "eosgi.testId", value = "resourceTest"),
+        @Property(name = "dataSource.target") })
+public class ResourceTestComponent {
+    /**
+     * Fake, non existing resourceId to be able to test deleteResource with wrong parameter.
+     */
+    private static final Long FAKE_ID = 2000L;
+    /**
+     * {@link ExpandedSQLTemplates}.
+     */
+    private static final SQLTemplates SQL_TEMPLATES = new ExpandedSQLTemplates();
+    /**
+     * Zero number.
+     */
+    private static final long ZERO = 0;
+    /**
+     * {@link ResourceService}.
+     */
+    @Reference(policy = ReferencePolicy.STATIC)
+    private ResourceService resourceService;
+    /**
+     * {@link DataSource}.
+     */
+    @Reference
+    private DataSource dataSource;
+
+    public void bindDataSource(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void bindResourceService(final ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    /**
+     * Counts the resource records in the database.
+     * 
+     * @return Number of the records.
+     */
+    private Long countResources() {
+        Long count = 0L;
+        try (Connection connection = dataSource.getConnection()) {
+            QResource qResource = new QResource("qResource");
+            SQLQuery countClause = new SQLQuery(connection, SQL_TEMPLATES);
+            count = countClause.from(qResource).count();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    /**
+     * Test method for {@link ResourceService} createResource and deleteResource methods.
+     */
+    @Test
+    @TestDuringDevelopment
+    public void testResourceService() {
+        Long firstCount = countResources();
+        long resourceId = resourceService.createResource();
+        Long count = countResources();
+        Long expectedCount = firstCount + 1;
+        Assert.assertEquals(expectedCount, count);
+        long numOfDeleted = resourceService.deleteResource(FAKE_ID);
+        count = countResources();
+        Assert.assertEquals(ZERO, numOfDeleted);
+        resourceService.deleteResource(resourceId);
+        count = countResources();
+        Assert.assertEquals(firstCount, count);
+    }
+}
